2b99ec4166530ec834fbd1d282c85e06e779d8a4
Dan Matthews <dan@hydrant.co.uk>
2013-09-18 14:09:14 +0100

    New lessons

--- InterpolationExample.php
+++ InterpolationExample.php
@@ -3,13 +3,12 @@
 // Variable interpolation is using {} curly braces to tell a string to pull in
 // the value of a variable.
 
-
 // VARIABLE INTERPOLATION ONLY WORKS WITH STRINGS INSIDE DOUBLE QUOTES.
 
 $name = "Dan Matthews";
 
 // Then, using double quotes, print it out:
-print "My Name is $name".PHP_EOL;
+print "String: My Name is $name".PHP_EOL;
 
 // Make a slightly more complicated variable, like an array:
 $data = array(
@@ -18,7 +17,12 @@ $data = array(
 );
 
 // For arrays, we need to use curly braces, otherwise it will not interpolate correctly.
-print "My name is {$data['name']}, and i am {$data['age']} years old.".PHP_EOL;
+print "Array: My name is {$data['name']}, and i am {$data['age']} years old.".PHP_EOL;
+
+// Quickly cast $data to be an object so that we can show you how objects work.
+$data = (object)$data;
+
+print "Object: My name is {$data->name}, and i am {$data->age} years old.".PHP_EOL;
 
 // Note how single quotes won't work at all:
-print 'My name is $dan'.PHP_EOL;
+print 'Single quotes: My name is $dan'.PHP_EOL;
